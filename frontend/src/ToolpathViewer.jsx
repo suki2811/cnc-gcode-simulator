a130148d37d233ec952dd2d2b4f8b0530b188d3b
suki2811 <sukeerthiadireddi@gmail.com>
2025-12-26 18:37:57 +0530

    Add toolpath viewer

---
+++ frontend/src/ToolpathViewer.jsx
@@ -0,0 +1,7 @@
+export default function ToolpathViewer() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <p>Toolpath visualization will appear here</p>
+    </div>
+  );
+}
